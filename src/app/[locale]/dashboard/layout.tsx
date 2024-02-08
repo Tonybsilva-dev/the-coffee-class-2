98ddeacce4701e9ef2062af5caf43da87171c7c9
Antonio Silva <antoniobsilvaprof@hotmail.com>
2024-02-08 08:54:57 -0300

    feat: improve dinamic title pages

--- src/app/[locale]/dashboard/layout.tsx
+++ src/app/[locale]/dashboard/layout.tsx
@@ -9,7 +9,9 @@ export default function DashboardLayout({
     <div className='lg:grid-cols-app min-h-screen lg:grid dark:bg-zinc-900'>
       Sidebar
       <div className='max-w-[100vw] px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pt-8'>
+        <div className="mb-2">
         <ScrollProgressHorizontal />
+        </div>
         {children}
       </div>
     </div>
